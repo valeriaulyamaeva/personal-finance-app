b2d2c5def7cec218bac06676c229e9ab4d26ca40
vale <leruntik2004@gmail.com>
2024-09-10 13:26:54 +0300

    подключили бд и проверили подключение

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"context"
 	"github.com/joho/godotenv"
-	"github.com/valeriaulyamaeva/personal-finance-appersonal-/internal/database"
+	"github.com/valeriaulyamaeva/personal-finance-app/internal/database"
 	"log"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
+	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
@@ -16,6 +16,12 @@ func main() {
 	if err != nil {
 		log.Fatal("Error connecting to database:", err)
 	}
-	defer conn.Close()
+	defer conn.Close(context.Background())
 
+	_, err = conn.Query(context.Background(), "SELECT 1")
+	if err != nil {
+		log.Fatal("Error executing test query:", err)
+	}
+
+	log.Println("Database connection established and test query successful")
 }
--- internal/database/db.go
+++ internal/database/db.go
@@ -1,1 +1,26 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+func ConnectDB() (*pgx.Conn, error) {
+	// Загрузить переменные из .env
+	if err := godotenv.Load(); err != nil {
+		return nil, fmt.Errorf("Error loading .env file")
+	}
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%d/%s",
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), 5432, os.Getenv("DB_NAME"))
+
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
