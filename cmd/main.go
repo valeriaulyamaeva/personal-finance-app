9bfd381409cb95589330571a4537bf86cfcc06c1
vale <leruntik2004@gmail.com>
2024-09-10 12:50:57 +0300

    добавление файлов

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,21 @@
-package cmd
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"github.com/valeriaulyamaeva/personal-finance-appersonal-/internal/database"
+	"log"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	conn, err := database.ConnectDB()
+	if err != nil {
+		log.Fatal("Error connecting to database:", err)
+	}
+	defer conn.Close()
+
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
